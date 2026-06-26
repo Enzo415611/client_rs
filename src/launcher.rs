52951b1cae53c5cbf905aa5794632ab4a5904e72
Enzo Queiroz Costa <enzo415611@gmail.com>
2026-06-26 16:25:26 -0300

    Wip: instances

--- src/launcher.rs
+++ src/launcher.rs
@@ -1,9 +1,11 @@
-use lighty_launcher::{
-    Authenticator, Launch, Loader,
-    core::{AppState},
-    version,
-};
-use std::{env::consts::OS, path::PathBuf};
+use lighty_launcher::launch::InstanceControl;
+use lighty_launcher::{Authenticator, Launch, Loader, event::EventBus, version};
+
+use parking_lot::Mutex;
+use slint::{ComponentHandle, Weak};
+use std::{env::consts::OS, path::PathBuf, sync::Arc};
+
+use crate::{AppState, AppWindow, Instance, Logic};
 
 pub fn get_minecraft_dir() -> PathBuf {
     match OS {
@@ -24,18 +26,81 @@ pub fn get_minecraft_dir() -> PathBuf {
     }
 }
 
-pub async fn init_launcher() -> anyhow::Result<()> {
-    println!("init minecraft");
-    
-    AppState::init(".minecraft")?;
+async fn run_new_instance(
+    weak: Weak<AppWindow>,
+    app_state: Arc<Mutex<AppState>>,
+    new_instance: Instance,
+) -> anyhow::Result<()> {
+    println!("init instance");
+    let event_bus = EventBus::new(1000);
+    let mut rx = event_bus.subscribe();
+    lighty_launcher::_core::AppState::init(".minecraft")?;
     let mut auth = lighty_launcher::auth::OfflineAuth::new("zzz");
-    let profile = auth.authenticate().await?;
-    let mut version = version::VersionBuilder::new("my-instance", Loader::Vanilla, "", "1.21.11");
+    let profile = auth.authenticate(Some(&event_bus)).await?;
 
-    version
+    let mut instance = version::VersionBuilder::new(
+        &new_instance.name,
+        new_instance.loader.clone(),
+        &new_instance.loader_version,
+        &new_instance.version,
+    );
+
+    app_state.lock().instances.push(Instance {
+        pid: instance.get_pid().unwrap_or_else(|| 0),
+        loader: new_instance.loader,
+        loader_version: new_instance.loader_version,
+        name: new_instance.name,
+        version: new_instance.version,
+        is_run: false
+    });
+
+
+    app_state.lock().instances_launched.push(instance.clone());
+
+    
+    slint::spawn_local(async_compat::Compat::new(async move {
+        let app_state = app_state.clone();
+        while let Ok(event) = rx.next().await {
+            match event {
+                lighty_launcher::event::Event::Launch(
+                    lighty_launcher::event::LaunchEvent::InstallProgress { bytes },
+                ) => {
+                    // progresso ao baixar os arquivos do jogo
+                    println!("   +{}", bytes);
+                }
+                lighty_launcher::event::Event::ConsoleOutput(out) => {
+                    // log do jogo ao inicar e ao sair
+
+                    println!("{}", out.line)
+                }
+                lighty_launcher::event::Event::Launch(
+                    lighty_launcher::event::LaunchEvent::Launched { version, pid },
+                ) => {                    
+                }
+                _ => {}
+            }
+        }
+    }))
+    .ok();
+
+    let instance_builder = instance
         .launch(&profile, lighty_launcher::JavaDistribution::Temurin)
+        .with_event_bus(&event_bus)
         .run()
         .await?;
 
     Ok(())
+}
+
+pub fn new_instance_thread(
+    weak: Weak<AppWindow>,
+    app_state: Arc<Mutex<AppState>>,
+    new_instance: Instance,
+) {
+    slint::spawn_local(async_compat::Compat::new(run_new_instance(
+        weak,
+        app_state,
+        new_instance,
+    )))
+    .ok();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,65 @@
 
 mod launcher;
 
-use std::error::Error;
+use std::{error::Error, sync::{Arc}};
 
-use tokio::task::{spawn_blocking, spawn_local};
 
-use crate::launcher::{get_minecraft_dir, init_launcher};
+
+use lighty_launcher::{Loader, VersionBuilder};
+use parking_lot::Mutex;
+use slint::{ModelRc, SharedString, VecModel, Weak};
+
+use crate::launcher::{get_minecraft_dir, new_instance_thread};
 
 slint::include_modules!();
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+struct AppState {
+    instances: Vec<VersionBuilder<Loader>>,
+}
+
+struct Instance {
+    pid: u32,
+    name: String,
+    version: String,
+    loader_version: String,
+    loader: Loader,
+    is_run: bool
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            instances: vec![],
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
-
-    let dir = get_minecraft_dir();
-    println!("{}", dir.display().to_string());
-
-    // deve estar dentro de uma thread async
-    // a ui não ira abrir a não ser que o jogo seja fechado
-    _ = init_launcher().await?;
+    let logic = ui.global::<Logic>();
+        
+    let app_state = Arc::new(Mutex::new(AppState::new()));
+    
+    let weak = ui.as_weak();
+    on_run_instance(weak, logic, app_state);
+    // new_instance_thread(weak, app_state.clone());
 
     ui.run()?;
     Ok(())
+}
+
+
+fn on_run_instance(weak: Weak<AppWindow>, logic: Logic, app_state: Arc<Mutex<AppState>>) {
+    logic.on_run_instance(move|instance| {
+        let new_instance = Instance {
+            pid: 0,
+            name: instance.name.to_string(),
+            version: instance.version.to_string(),
+            loader: Loader::Vanilla,
+            loader_version: instance.loader_version.to_string(),
+            is_run: true
+        };
+        
+        new_instance_thread(weak.clone(), app_state.clone(), new_instance);
+    });
 }
